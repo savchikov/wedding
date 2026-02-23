3f9fb8abb9c596268c3a88e7736432ae68ae3661
savchikov <127405608+savchikov@users.noreply.github.com>
2026-02-23 12:13:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-// ================== КОНФИГУРАЦИЯ TELEGRAM БОТА ==================
-
-// Токен бота (получить у @BotFather в Telegram)
 const TELEGRAM_BOT_TOKEN = "7636378961:AAGLS9pWpMEV_l8nRRfuQ7DNiI0XfCArVRk";
-
-// ID чата для отправки уведомлений (получить у @userinfobot)
 const TELEGRAM_CHAT_ID = "-4977417118";
+
 
